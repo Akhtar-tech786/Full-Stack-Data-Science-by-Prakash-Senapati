d5d1ce0b1c9428342238577909636bdb8d0806ce
Akhtar-tech786 <79236032+Akhtar-tech786@users.noreply.github.com>
2026-06-19 18:24:26 +0530

    updated

--- 2. Python for Data Science/Seaborn/app.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write('''welcome to NIT & appreciate the student who work with me & you guys are awesome you dont watch ipl and you work with me, you guys are rocking''')
